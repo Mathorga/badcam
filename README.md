44a8ef3d8ab2f70c1cf100f7a563b104fdce50e5
Luka Micheletti <michelettiluka@gmail.com>
2024-07-20 17:15:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Compile
+g++ -std=c++17 -Wall -pedantic -g -I/usr/local/include -c ./badcam.cpp -o ./badcam.o -I/usr/include/opencv4
+
+# Link
+g++ -Wall ./badcam.o -o ./badcam -lm -lrt -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio
---
+++ badcam.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+int main(int argc, char **argv) {
+    cv::Mat frame;
+    cv::VideoCapture cam(0);
+
+    // cam.open(0);
+    if (!cam.isOpened()) {
+        printf("ERROR! Unable to open camera\n");
+        return -1;
+    }
+
+    // Create a window for display.
+    cv::namedWindow("badcam", cv::WINDOW_FULLSCREEN);
+
+    while (cam.read(frame)) {
+        if (frame.empty()) {
+            printf("ERROR! blank frame grabbed\n");
+            break;
+        }
+
+        cv::imshow("badcam", frame);
+        cv::pollKey();
+    }
+
+    return 0;
+}
