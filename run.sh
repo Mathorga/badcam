ba67559b44aadd4b9794d2e26cc4b6b3d8f28d83
Luka Micheletti <michelettiluka@gmail.com>
2024-07-22 21:54:50 +0100

    Added build specs

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ mkdir ./bld
 mkdir ./bin
 
 g++ -std=c++17 -Wall -pedantic -g -I/usr/local/include -c ./badcam.cpp -o ./bld/badcam.o -I/usr/include/opencv4
-g++ -Wall -L/usr/local/lib ./bld/badcam.o -o ./bin/badcam -lm -lrt -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lraylib
+g++ -Wall -L/usr/local/lib ./bld/badcam.o -o ./bin/badcam -Wl,--copy-dt-needed-entries -lm -ldl -lrt -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -lopencv_videoio -lraylib
 ./bin/badcam
