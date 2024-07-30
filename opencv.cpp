873d6a503988e2f7a61ca6b2cd96d5bcc244352a
Luka Micheletti <l.micheletti@ebw.it>
2024-07-30 12:20:00 +0200

    Moved fullscreen set

--- opencv.cpp
+++ opencv.cpp
@@ -58,6 +58,9 @@ int main(int argc, char **argv) {
         cv::imshow("badcam", frame);
         pressed_key = (char) cv::waitKey(25);
 
+        // Unconditionally try and update the window to fullscreen.
+        cv::setWindowProperty("badcam", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+
         if (pressed_key == 27) {
             // ESC key was pressed.
             break;
@@ -82,9 +85,6 @@ int main(int argc, char **argv) {
             // Save frame to file.
             cv::imwrite("/home/luka/Desktop/capture" + std::to_string(millis_since_epoch) + ".jpg", capture_frame);
         }
-
-        // Unconditionally try and update the window to fullscreen.
-        cv::setWindowProperty("badcam", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
     }
 
     return 0;
